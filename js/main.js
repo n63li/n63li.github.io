59fd7592b3151c2833cc252938ed92d496ac5cdc
Nathan Li <n63li@edu.uwaterloo.ca>
2018-12-13 23:36:39 -0500

    Work scrolling animations working.

--- js/main.js
+++ js/main.js
@@ -54,3 +54,27 @@ anime.timeline({loop: true})
 $(window).scroll(function(){
   $(".arrow.bounce").css("opacity", 1 - $(window).scrollTop() / 30);
 });
+
+let dots          = 4;
+let sliderElem    = document.querySelector('.slider')
+let dotElems      = sliderElem.querySelectorAll('.slider__dot')
+let indicatorElem = sliderElem.querySelector('.slider__indicator')
+
+Array.prototype.forEach.call(dotElems, (dotElem) => {
+
+  dotElem.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let currentPos = parseInt(sliderElem.getAttribute('data-pos'))
+    let newPos     = parseInt(dotElem.getAttribute('data-pos'))
+
+    let newDirection     = (newPos > currentPos ? 'right' : 'left')
+    let currentDirection = (newPos < currentPos ? 'right' : 'left')
+
+    indicatorElem.classList.remove(`slider__indicator--${ currentDirection }`)
+    indicatorElem.classList.add(`slider__indicator--${ newDirection }`)
+    sliderElem.setAttribute('data-pos', newPos)
+
+  })
+
+})
